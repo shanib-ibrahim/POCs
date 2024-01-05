58b967a57677d7b1733350de89a5837bceda8453
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2024-01-05 09:01:45 +0530

    Added Insert , Edit and Delete API for Todo App

--- todo-app/src/app/api/todolist/[id]/route.ts
+++ todo-app/src/app/api/todolist/[id]/route.ts
@@ -1,10 +1,51 @@
 import { NextResponse } from "next/server";
-import { tasks } from "../../../../todo.json";
+import todo from "../../../../todo.json";
+import fs from "fs";
 
 export const GET = async (
   request: Request,
   { params }: { params: { id: number } }
 ) => {
-  const task = tasks.filter((task) => task.id === Number(params.id));
+  const task = todo.tasks.filter((task) => task.id === Number(params.id));
   return NextResponse.json(task, { status: 200 });
+};
+
+export const DELETE = async (
+  request: Request,
+  { params }: { params: { id: number } }
+) => {
+  let msg = "Successfully deleted the task";
+  let status = 200;
+  const data = {
+    tasks: todo.tasks.filter((task) => task.id !== Number(params.id)),
+  };
+  await fs.writeFile("./src/todo.json", JSON.stringify(data), (err) => {
+    if (err) {
+      msg = "Error in deleting the tasks :" + err;
+      status = 500;
+    }
+  });
+  return NextResponse.json(msg, { status });
+};
+
+export const PUT = async (
+  request: Request,
+  { params }: { params: { id: number } }
+) => {
+  let msg = "Successfully edited the task";
+  let status = 200;
+  const taskData = await request.json();
+  todo.tasks.forEach((task) => {
+    if (task.id === Number(params.id)) {
+      task.text = taskData.text;
+    }
+  });
+  const data = { tasks: todo.tasks };
+  await fs.writeFile("./src/todo.json", JSON.stringify(data), (err) => {
+    if (err) {
+      msg = "Error in editing the tasks :" + err;
+      status = 500;
+    }
+  });
+  return NextResponse.json(msg, { status });
 };
--- todo-app/src/app/api/todolist/route.ts
+++ todo-app/src/app/api/todolist/route.ts
@@ -1,6 +1,22 @@
 import { NextResponse } from "next/server";
-import { tasks } from "../../../todo.json";
+import todo from "../../../todo.json";
+import fs from "fs";
 
 export const GET = async (request: Request) => {
-  return NextResponse.json(tasks, { status: 200 });
+  return NextResponse.json(todo.tasks, { status: 200 });
 };
+
+export async function POST(request: Request) {
+  let msg = "Successfully added the task";
+  let status = 200;
+  const task = await request.json();
+  todo.tasks.push(task);
+  const data = { tasks: todo.tasks };
+  await fs.writeFile("./src/todo.json", JSON.stringify(data), (err) => {
+    if (err) {
+      msg = "Error in adding the tasks :" + err;
+      status = 500;
+    }
+  });
+  return NextResponse.json(msg, { status });
+}
