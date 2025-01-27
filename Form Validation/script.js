c971d68ada5278d7c9be6e2fd45fd342a57fd6d3
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-01-27 06:59:53 +0400

    changes in script

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -1,8 +1,28 @@
 const form = document.getElementById("form");
-const userName = document.getElementById("username");
+const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const cpassword = document.getElementById("confirm-password");
 const submit = document.getElementById("btn");
 
-form.addEventListener("submit", () => {});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+});
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const cpasswordValue = cpassword.value.trim();
+
+  if (userName === "") {
+    setError(userName, "Username is Required");
+  } else {
+    setSuccess(username);
+  }
+};
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+};
