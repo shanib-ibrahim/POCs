1522e4bd67ae899dbed53505e44995dbcea84c5d
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-01-27 06:50:22 +0400

    initial commit of registration form

---
+++ Form Validation/script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const cpassword = document.getElementById("confirm-password");
+const submit = document.getElementById("btn");
+
+form.addEventListener("submit", () => {});
