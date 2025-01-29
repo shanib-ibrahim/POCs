9ec90249ee2e31360cea9c0e040dec789caefb33
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-01-29 06:53:10 +0400

    changes in filter product

--- filter-product/script.js
+++ filter-product/script.js
@@ -4,12 +4,30 @@ const searchBtn = document.getElementById("search-btn");
 const categoryBtns = document.querySelectorAll(".category-btn");
 
 const filterProduct = () => {
-  const searchValue = searchInput.ariaValueMax.toLocaleLowerCase();
+  const searchValue = searchInput.value.toLocaleLowerCase();
   const productItems = document.querySelectorAll(".product-item");
-  productItems.forEach(item=>{
-    const title = 
-  })
+  const activeCategory = document.querySelector(".category-btn.active").dataset
+    .category;
+  productItems.forEach((item) => {
+    const title = item.querySelector("h3").innerText.toLocaleLowerCase();
+    const category = item.dataset.category;
+
+    if (
+      (title.includes(searchValue) || searchValue === "") &&
+      (category === activeCategory || activeCategory === "all")
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 };
+
+function setCategory(e) {
+  categoryBtns.forEach((btn) => btn.classList.remove("active"));
+  e.target.classList.add("active");
+  filterProduct();
+}
 
 searchBtn.addEventListener("click", filterProduct);
 searchInput.addEventListener("keyup", filterProduct);
