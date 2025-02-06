d666a837a5fe0d2ac668bd3879aa672feca265a9
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-06 07:05:01 +0400

    changes in slider

--- image-slider/script.js
+++ image-slider/script.js
@@ -1,1 +1,33 @@
 let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const totalSlides = slides.length;
+
+const showSlides = () => {
+  if (slideIndex < 0) {
+    slideIndex = totalSlides - 1;
+  } else if (slideIndex >= totalSlides) {
+    slideIndex = 0;
+  }
+  slides.forEach((slide, index) => {
+    if (slideIndex === index) {
+      slide.style.display = "block";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+};
+
+const prevSlide = () => {
+  slideIndex--;
+  showSlides();
+};
+
+const nextSlide = () => {
+  slideIndex++;
+  showSlides();
+};
+
+prev.addEventListener("click", prevSlide);
+next.addEventListener("click", nextSlide);
