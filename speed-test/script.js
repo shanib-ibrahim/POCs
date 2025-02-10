4a3bc3aedb1115ce43a3ba941a57a1b72e4aac9f
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-10 05:48:40 +0400

    changes in typing speed test app

---
+++ speed-test/script.js
@@ -0,0 +1,7 @@
+const typingText = document.querySelector(".typing text p");
+const input = document.querySelector(".wrapper .input-field");
+const time = document.querySelector(".time span b");
+const mistakes = document.querySelector(".mistakes span");
+const wpm = document.querySelector(".wpm span");
+const cpm = document.querySelector(".cpm span");
+const btn = document.querySelector(".button");
