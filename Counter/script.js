02bdf70621f742659f1c6bc1bfeb13217050db41
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-11 07:35:53 +0400

    changes in counter

--- Counter/script.js
+++ Counter/script.js
@@ -1,3 +1,30 @@
 const counter = document.getElementById("counter");
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+
+let count = 0;
+
+increase.addEventListener("click", () => {
+  count++;
+  counter.innerText = count;
+  increase.style.border = "1px solid green";
+  decrease.style.border = "1px solid black";
+});
+
+decrease.addEventListener("click", () => {
+  count--;
+  if (count < 0) {
+    count = 0;
+  }
+  counter.innerText = count;
+  decrease.style.border = "1px solid red";
+  increase.style.border = "1px solid black";
+});
+
+reset.addEventListener("click", () => {
+  count = 0;
+  counter.innerText = count;
+  decrease.style.border = "1px solid black";
+  increase.style.border = "1px solid black";
+});
