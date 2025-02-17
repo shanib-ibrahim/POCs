2d518f40611a17bf508af39979bbece2a2cf9a10
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-17 06:21:11 +0400

    changes in navbar styles

--- navbar/script.js
+++ navbar/script.js
@@ -1,7 +1,1 @@
-const hamburger = document.getElementById("hamburger");
 
-hamburger.addEventListener("click", (e) => {
-  Array.from(hamburger.children).forEach((item) => {
-    item.classList.toggle("click");
-  });
-});
