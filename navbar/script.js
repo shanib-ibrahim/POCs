1a105b46f04f6386d99c1a4e74ca1c9d6fe248a2
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-17 07:49:03 +0400

    changes in navbar

--- navbar/script.js
+++ navbar/script.js
@@ -1,1 +1,6 @@
+const offscreen = document.getElementById("offscreen");
+const hamburger = document.getElementById("check");
 
+hamburger.addEventListener("click", () => {
+  offscreen.classList.toggle("active");
+});
