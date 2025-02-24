89bdfd38c7d9aa644e0e7483b19118d4aa2a3bf0
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-24 06:36:04 +0400

    changes in faq script

--- FAQ Page/script.js
+++ FAQ Page/script.js
@@ -1,7 +1,9 @@
 const openButtons = document.querySelectorAll(".open-btn");
 const faqAnswer = document.querySelectorAll(".faq-answer");
+
 openButtons.forEach((openbtn, index) => {
-  openbtn.addEventListener("click", () => {
+  openbtn.addEventListener("click", (e) => {
     faqAnswer[index].classList.toggle("open");
+    e.target.classList.toggle("open");
   });
 });
