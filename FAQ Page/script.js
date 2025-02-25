34bfd2e4e781048f1bff949c783bbf02731330d5
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-02-25 06:16:36 +0400

    changes in faq pages

--- FAQ Page/script.js
+++ FAQ Page/script.js
@@ -4,6 +4,6 @@ const faqAnswer = document.querySelectorAll(".faq-answer");
 openButtons.forEach((openbtn, index) => {
   openbtn.addEventListener("click", (e) => {
     faqAnswer[index].classList.toggle("open");
-    e.target.classList.toggle("open");
+    openButtons[index].classList.toggle("open");
   });
 });
