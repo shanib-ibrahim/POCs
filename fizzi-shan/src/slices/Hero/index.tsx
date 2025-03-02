4230c1634d81a0a76f1c44ff8fec8d0d1457d0eb
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-03-02 12:22:12 +0400

    changes to add gsap to the project

--- fizzi-shan/src/slices/Hero/index.tsx
+++ fizzi-shan/src/slices/Hero/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC } from "react";
 import { asText, Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
@@ -5,6 +6,11 @@ import { PrismicNextImage } from "@prismicio/next";
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Props for `Hero`.
@@ -15,11 +21,55 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+    introTl
+      .set(".hero", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1,
+      })
+      .from(".hero-subheading", { opacity: 0, y: 30 }, "+=.80")
+      .from(".hero-body", { opacity: 0, y: 10 })
+      .from(".hero-button", { opacity: 0, y: 10, duration: 0.6 });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTl
+      .fromTo(
+        "body",
+        { backgroundColor: "#FDE047" },
+        { backgroundColor: "#D9F99F", overwrite: "auto" },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 4.0,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", { y: 20, opacity: 0 });
+  });
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="hero"
+      className="hero opacity-0"
     >
       <div className="grid">
         <div className="grid h-screen place-items-center">
