c8c217eb1d8a4e723f502ef60781bb67f3665900
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-03-02 10:17:13 +0400

    initial commit of learning gsap,three.js and prism

---
+++ fizzi-shan/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+import Header from "@/components/Header";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-alpino",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${alpino.variable} `}>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        {children}
+        <main></main>
+      </body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
