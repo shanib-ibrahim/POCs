ceac4d0fc6f58fa0fe9c3d90ddcf146deca9430c
Shanib Ibrahim <shanibibrahimcm@gmail.com>
2025-03-18 07:16:22 +0400

    changes in scene in sky dive

--- fizzi-shan/src/slices/SkyDive/Scene.tsx
+++ fizzi-shan/src/slices/SkyDive/Scene.tsx
@@ -2,13 +2,7 @@
 
 import FloatingCan from "@/components/FloatingCan";
 import { Content } from "@prismicio/client";
-import {
-  Cloud,
-  Clouds,
-  Environment,
-  OrbitControls,
-  Text,
-} from "@react-three/drei";
+import { Cloud, Clouds, Environment, Text } from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 import gsap from "gsap";
@@ -36,7 +30,7 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
   const getXPosition = (distance: number) => distance * Math.cos(ANGLE);
   const getYPosition = (distance: number) => distance * Math.sin(ANGLE);
 
-  const getXYPosition = (distance: number) => ({
+  const getXYPositions = (distance: number) => ({
     x: getXPosition(distance),
     y: getYPosition(-1 * distance),
   });
@@ -52,10 +46,10 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
       return;
 
     gsap.set(cloudsRef.current.position, { z: 10 });
-    gsap.set(canRef.current.position, { ...getXYPosition(-4) });
+    gsap.set(canRef.current.position, { ...getXYPositions(-4) });
     gsap.set(
       wordsRef.current.children.map((word) => word.position),
-      { ...getXYPosition(7), z: 2 },
+      { ...getXYPositions(7), z: 2 },
     );
     gsap.to(canRef.current.rotation, {
       y: Math.PI * 2,
@@ -63,6 +57,70 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
       repeat: -1,
       ease: "none",
     });
+
+    const DISTANCE = 15;
+    const DURATION = 6;
+
+    gsap.set([cloud2Ref.current.position, cloud1Ref.current.position], {
+      ...getXYPositions(DISTANCE),
+    });
+
+    gsap.to(cloud1Ref.current.position, {
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      x: `+=${getXPosition(DISTANCE * -2)}`,
+      ease: "none",
+      repeat: -1,
+      duration: DURATION,
+    });
+
+    gsap.to(cloud2Ref.current.position, {
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      x: `+=${getXPosition(DISTANCE * -2)}`,
+      ease: "none",
+      repeat: -1,
+      delay: DURATION / 2,
+      duration: DURATION,
+    });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".skydive",
+        pin: true,
+        start: "top top",
+        end: "+=2000",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      .to("body", {
+        backgroundColor: "#C0F0F5",
+        overwrite: "auto",
+        duration: 0.1,
+      })
+      .to(cloudsRef.current.position, { z: 0, duration: 0.3 }, 0)
+      .to(canRef.current.position, {
+        x: 0,
+        y: 0,
+        duration: 0.3,
+        ease: "back.out(1.7)",
+      })
+      .to(
+        wordsRef.current.children.map((word) => word.position),
+        {
+          keyframes: [
+            { x: 0, y: 0, z: -1 },
+            { ...getXYPositions(7), z: -7 },
+          ],
+          stagger: 0.3,
+        },
+        0,
+      )
+      .to(canRef.current.position, {
+        ...getXYPositions(4),
+        duration: 0.5,
+        ease: "back.in(1.7)",
+      });
   });
 
   return (
@@ -87,8 +145,6 @@ export default function Scene({ sentence, flavor }: SkyDiveProps) {
       <group ref={wordsRef}>
         {sentence && <ThreeText sentence={sentence} color="#F97315" />}
       </group>
-
-      <OrbitControls />
 
       <ambientLight intensity={2} color="#9ddefa" />
       <Environment files="/hdr/field.hdr" environmentIntensity={1.5} />
